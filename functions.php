5893b23a3a55171510b65bf7bedb2e608a4cfe3c
Ricard Torres <ricard@ricard.dev>
2024-03-10 12:17:05 +0100

    Remove sidebar

--- functions.php
+++ functions.php
@@ -76,26 +76,6 @@ function drinks_setup() {
 add_action( 'after_setup_theme', 'drinks_setup' );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function drinks_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'drinks' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'drinks' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'drinks_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function drinks_scripts() {
